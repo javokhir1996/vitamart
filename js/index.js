b2a9507fc8f6ca92cf12d43f38b6c5bd5acc8699
javokhir1996 <qochqorov1996@inbox.ru>
2021-05-01 00:55:54 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.onetime').slick({
+    dots: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    touchMove: false,
+    autoplay: true,
+    });
+});
+
+// otziv slider
+$('.carousel').owlCarousel({  
+});
+
+
